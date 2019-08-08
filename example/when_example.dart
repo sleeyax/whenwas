232451047c743d191f8353e6a93a22316b354057
Sleeyax <yourd3veloper@gmail.com>
2019-08-08 19:07:08 +0200

    Added example

---
+++ example/when_example.dart
@@ -0,0 +1,16 @@
+import 'package:when/when.dart';
+
+main() {
+  var dateInThePast = DateTime.now().subtract(
+    Duration(
+      milliseconds: 2,
+      seconds: 15,
+      minutes: 27,
+      hours: 4,
+      days: 29
+    )
+  );
+
+  // 29 days ago
+  print(When(dateInThePast));
+}
