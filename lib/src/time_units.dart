82b03dbaffb505a7a2a2e361803c23568bd350ea
Sleeyax <yourd3veloper@gmail.com>
2019-08-16 13:38:40 +0200

    Move time units to file

---
+++ lib/src/time_units.dart
@@ -0,0 +1,9 @@
+class TimeUnit {
+  static const YEAR        = 'year';
+  static const MONTH       = 'month';
+  static const DAY         = 'day';
+  static const HOUR        = 'hour';
+  static const MINUTE      = 'minute';
+  static const SECOND      = 'second';
+  static const MILLISECOND = 'millisecond';
+}
